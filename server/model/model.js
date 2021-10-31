553974918cab09f241ae51f32b97b8908aba08f6
SaurabhKumar3 <saurabhsahani546@gmail.com>
2021-10-31 15:58:24 +0530

    first commit

---
+++ server/model/model.js
@@ -0,0 +1,19 @@
+const mongoose =require('mongoose')
+
+var schema= new mongoose.Schema({
+
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    gender:String,
+    status:String
+})
+
+const userdb = mongoose.model('userdb',schema)
+
+module.exports=userdb
